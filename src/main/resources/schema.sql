85ca4fd463eab220cfff7cbb2707cddc614d3ce0
piotr kotynia <piotr.kotynia@gmail.com>
2023-10-24 19:20:48 +0200

    car rental

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE car_rental (
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    internet_domain  VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL
+);
