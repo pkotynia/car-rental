76a00ee2359c3658f373f9750d002cda6e3b99d3
Paweł Jurkiewicz <pablo.jurkiewicz.vip@gmail.com>
2023-11-21 17:18:46 +0100

    Reservation Service test

---
+++ src/main/java/com/sda/carrental/configuration/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.sda.carrental.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Create UserDetailsManager to handle users
+
+    //Create and save users using Initializer Bean
+
+    //SecurityFilterChain
+
+}
