ef48c2885d8ef2b6688f8a74bd4b5388ddc5703c
piotr kotynia <piotr.kotynia@gmail.com>
2023-11-21 18:39:58 +0100

    comments added to tests

--- src/test/java/com/sda/carrental/car_rental_facility/CarRentalControllerTest.java
+++ src/test/java/com/sda/carrental/car_rental_facility/CarRentalControllerTest.java
@@ -7,14 +7,19 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
+// This is an example of integration test
+// We are not using any mocks.
+// Application is being run and we use WebTestClient to send the request and assert responses
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarRentalControllerTest {
 
     @Autowired
+    //WebTestClient allows us to send request and verify responses
     private WebTestClient testClient;
 
     @Test
     void shouldSaveCarRental() {
+        //given - preparing objects that will be used in request body (payload)
         List<CompanyBranchModel> branches = List.of(
             new CompanyBranchModel(null, "Radom")
         );
@@ -28,13 +33,13 @@ class CarRentalControllerTest {
 
         );
         testClient
-                //given
+                //given - request preparation
                 .post()
                 .uri("/car_rentals")
                 .bodyValue(carRental)
-                //when
+                //when - send the request to our app
                 .exchange()
-                //then
+                //then - assert response
                 .expectStatus()
                 .isOk();
     }
